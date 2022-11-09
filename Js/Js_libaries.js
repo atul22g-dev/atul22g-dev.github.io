f195e43464f86277efdc4027890a67aeae250437
Atugatran <atugatran@gmail.com>
2022-11-10 00:45:52 +0530

    improve in js-libary & add api-container

--- Js/Js_libaries.js
+++ Js/Js_libaries.js
@@ -13,10 +13,31 @@ function loadLibaries() {
   let Libaries_container = document.getElementById("Libaries-container");
   let Libarry_item = Libarry.map((i) => {
     return `
-    <li class="collection-item" key=${i.id}>
-          <a target="_blank" href=${i.src}>${i.name}</a>
-    </li>
+    <li class="cards__item" key=${i.id}>
+    <div class="card__body">
+      <div class="card__image">
+        <img src="${i.img}" alt="">
+      </div>
+      <div class="card__content">
+        <div class="card__title">${i.name}</div>
+        <div class="card__links">
+          <a class="card__btn" href="${i.demo}" target="_blank">
+            <span>Live Demo</span>
+          </a>
+          <a class="card__btn" href="${i.src}"
+            target="_blank">
+            <span>Source Code</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>
   `;
+
+  // <li class="Libaries-item" key=${i.id}>
+  //         <a target="_blank" href=${i.src}>${i.name}</a>
+  // </li>
+
   });
   Libaries_container.innerHTML = Libarry_item.join("");
 }
