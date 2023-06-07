83bea5bf9b8b56cafef85ac444c8bad85e979db1
Atul <atul22g857086@gmail.com>
2023-06-07 06:07:36 +0530

    Add Repo, Libary

---
+++ Js/Repositories.js
@@ -0,0 +1,38 @@
+let Repositories = null;
+
+fetch('https://atugatran-projects.github.io/_Apis/Repositories/index.json')
+  .then(res => res.json())
+  .then(result => {
+    Repositories = result;
+    loadRepo();
+  })
+
+// NOTE
+// i'm gonna parse the url so my websites json data is not effected;
+function loadRepo() {
+  let Repositories_container = document.getElementById('Repositories-container');
+  let card = Repositories.map((i) => {
+    return `
+    <li class="cards__item" key=${i.id}>
+    <div class="card__body">
+      <div class="card__image">
+        <img src="${i.img}" alt="">
+      </div>
+      <div class="card__content">
+        <div class="card__title">${i.title}</div>
+        <div class="card__links">
+          <a class="card__btn" href="${i.demo}" target="_blank">
+            <span>Live Demo</span>
+          </a>
+          <a class="card__btn" href="${i.src}"
+            target="_blank">
+            <span>Source Code</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>
+  `
+  });
+  Repositories_container.innerHTML = card.join('');
+}
---
+++ Js/libaries.js
@@ -0,0 +1,38 @@
+let libaries = null;
+
+fetch('https://atugatran-projects.github.io/_Apis/Js_Libaries/index.json')
+  .then(res => res.json())
+  .then(result => {
+    libaries = result;
+    loadLib();
+  })
+
+// NOTE
+// i'm gonna parse the url so my websites json data is not effected;
+function loadLib() {
+  let libaries_container = document.getElementById('Libaries-container');
+  let card = libaries.map((i) => {
+    return `
+    <li class="cards__item" key=${i.id}>
+    <div class="card__body">
+      <div class="card__image">
+        <img src="${i.img}" alt="">
+      </div>
+      <div class="card__content">
+        <div class="card__title">${i.title}</div>
+        <div class="card__links">
+          <a class="card__btn" href="${i.demo}" target="_blank">
+            <span>Live Demo</span>
+          </a>
+          <a class="card__btn" href="${i.src}"
+            target="_blank">
+            <span>Source Code</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>
+  `
+  });
+  libaries_container.innerHTML = card.join('');
+}
