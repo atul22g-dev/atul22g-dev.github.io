392dc51de2697b581746606a7434dc48402e427b
atul <atul22gcoders@gmail.com>
2024-01-26 16:14:51 +0530

    Add app file

---
+++ Js/application.js
@@ -0,0 +1,38 @@
+let application = null;
+
+fetch('https://atugatranapi.pages.dev/App/index.json')
+    .then(res => res.json())
+    .then(result => {
+        application = result;
+        loadApp();
+    })
+
+// NOTE
+// i'm gonna parse the url so my websites json data is not effected;
+function loadApp() {
+    let application_container = document.getElementById('application-container');
+    let app = application.map((i) => {
+        return `
+    <li class="cards__item" key=${i.id}>
+    <div class="card__body">
+      <div class="card__image">
+        <img src="${i.img}" alt="">
+      </div>
+      <div class="card__content">
+        <div class="card__title">${i.title}</div>
+        <div class="card__links">
+          <a class="card__btn" href="${i.demo}" target="_blank">
+            <span>Download</span>
+          </a>
+          <a class="card__btn" href="${i.src}"
+            target="_blank">
+            <span>Source Code</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </li>
+  `
+    });
+    application_container.innerHTML = app.join('');
+}
